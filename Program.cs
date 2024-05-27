6d0a56a53f5ad7a8af4f5c0467eafa73d2bd6be5
Guilherme Almeida <113144349+Guilhermecrk@users.noreply.github.com>
2024-05-27 15:36:21 -0300

    Desafio Feito!!

--- Program.cs
+++ Program.cs
@@ -3,22 +3,23 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+//  IMPLEMENTADO !!!!!
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Guilherme", sobrenome: "Almeida");
+Pessoa p2 = new Pessoa(nome: "Pablo", sobrenome: "Escobar");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 50);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 7);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
+
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
